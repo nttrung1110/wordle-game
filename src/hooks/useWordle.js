ab96762152b3a0e5abbaca888518a74acdfa9131
NguyenTienTrung <4501104262@student.hcmue.edu.vn>
2022-06-14 16:41:22 +0700

    Submitting Guesses

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -9,7 +9,9 @@ const useWordle = (solution) => {
 
   // format a guess into an array of letter objects
   // e.g. [{key: 'a', color: 'yellow'}]
-  const formatGuess = () => {};
+  const formatGuess = () => {
+    console.log(currentGuess);
+  };
 
   // add a new guess to the guesses state
   // update the isCorrect state if the guess is correct
@@ -19,6 +21,29 @@ const useWordle = (solution) => {
   // handle keyup event & track current guess
   // if user presses enter, add the new guess
   const handleKeyup = ({ key }) => {
+    if (key === "Enter") {
+      // only add guess if turn is less than 5
+      if (turn > 5) {
+        console.log("you used all your guesses!");
+        return;
+      }
+
+      // do not allow duplicate words
+      if (history.includes(currentGuess)) {
+        console.log("you already tried that word.");
+        return;
+      }
+
+      // check word is 5 chars
+      if (currentGuess.length !== 5) {
+        console.log("word must be 5 chars.");
+        return;
+      }
+
+      // all conditions pass then format guess
+      formatGuess();
+    }
+
     if (key === "Backspace") {
       setCurrentGuess((prev) => prev.slice(0, -1));
       return;
