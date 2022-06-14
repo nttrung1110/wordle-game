00b2fcfbe59992bd1a0bd844df82c37938276611
NguyenTienTrung <4501104262@student.hcmue.edu.vn>
2022-06-14 16:34:41 +0700

    Custom hook & Tracking the Current Guess

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Wordle from "./components/Wordle";
 
 const App = () => {
   const [solution, setSolution] = useState(null);
@@ -17,7 +18,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Wordle (Lingo)</h1>
-      {solution && <div>Solution is: {solution}</div>}
+      {solution && <Wordle solution={solution} />}
     </div>
   );
 };
